d971f14469cb69bded5636c03f1710ddd6f7bfbd
0xWaleed <imwaleed@outlook.sa>
2023-07-11 12:28:28 +0300

    refactor(coords): add random locations as example :recycle:

--- 0xspawn-coords/coords.lua
+++ 0xspawn-coords/coords.lua
@@ -3,54 +3,52 @@
 --- DateTime: 7/4/23 6:09 AM
 ---
 
-loc 'store-1' {
-    x = -1299.76,
-    y = -880.85,
-    z = 11.9,
-    heading = 20.36,
-    model = 'player_two',
-    imageUrl = 'https://picsum.photos/1920/1080',
-    label = 'Store One'
-}
-
-loc 'store-2' {
-    x = -1275.11,
-    y = -860.88,
-    z = 12.22,
-    heading = 301.76,
+loc 'random-1' {
+    x = -3058.714,
+    y = 3329.19,
+    z = 12.5844,
+    heading = 0,
     model = 'player_zero',
-    imageUrl = 'https://picsum.photos/1921/1081',
-    label = 'Store Two'
-}
-
-loc 'store-3' {
-    x = -3058.714, y = 3329.19, z = 12.5844,
-    heading = 112.63,
-    model = 'player_one',
-    imageUrl = 'https://picsum.photos/1922/1082',
+    imageUrl = 'https://picsum.photos/512?a=1',
     label = 'ZancudoBunker'
 }
 
-loc 'store-4' {
-    x = -1192.75,
-    y = -827.75,
-    z = 14.34,
-    heading = 352.63,
-    model = 'a_m_m_bevhills_02',
-    imageUrl = 'https://picsum.photos/1923/1083',
-    label = 'Store Four'
+loc 'random-2' {
+    x = 24.43542,
+    y = 2959.705,
+    z = 58.35517,
+    heading = 0,
+    model = 'player_one',
+    imageUrl = 'https://picsum.photos/512?a=2',
+    label = 'Route68Bunker'
 }
 
+loc 'random-3' {
+    x = 481.0465,
+    y = 2995.135,
+    z = 43.96672,
+    heading = 0,
+    model = 'player_one',
+    imageUrl = 'https://picsum.photos/512?a=3',
+    label = 'OilfieldsBunker'
+}
 
-for i = 1, 5 do
-    local width = 1084 + i
-    loc('random-' .. i) {
-        x = -1192.75,
-        y = -827.75,
-        z = 14.34,
-        heading = 352.63,
-        model = 'a_m_m_bevhills_02',
-        imageUrl = 'https://picsum.photos/1923/' .. width,
-        label = 'Store ' .. i
-    }
-end
+loc 'random-4' {
+    x = 848.6175,
+    y = 2996.567,
+    z = 45.81612,
+    heading = 0,
+    model = 'player_two',
+    imageUrl = 'https://picsum.photos/512?a=4',
+    label = 'DesertBunker'
+}
+
+loc 'random-5' {
+    x = 2126.785,
+    y = 3335.04,
+    z = 48.21422,
+    heading = 0,
+    model = 'player_zero',
+    imageUrl = 'https://picsum.photos/512?a=5',
+    label = 'SmokeTreeBunker'
+}
