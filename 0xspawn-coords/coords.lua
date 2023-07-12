14a5350dc8bfe6fc4248bcc4ee9b405c6d52307d
0xWaleed <imwaleed@outlook.sa>
2023-07-12 13:02:58 +0300

    refactor(coords): keep only two locations for better example :recycle:

--- 0xspawn-coords/coords.lua
+++ 0xspawn-coords/coords.lua
@@ -4,51 +4,21 @@
 ---
 
 loc 'random-1' {
-    x = -3058.714,
-    y = 3329.19,
-    z = 12.5844,
-    heading = 0,
+    x = -2508.93,
+    y = 3300.59,
+    z = 91.96,
+    heading = 226.19,
     model = 'player_zero',
     imageUrl = 'https://picsum.photos/512?a=1',
-    label = 'ZancudoBunker'
+    label = 'North Chumash'
 }
 
 loc 'random-2' {
-    x = 24.43542,
-    y = 2959.705,
-    z = 58.35517,
-    heading = 0,
+    x = -78.32,
+    y = -815.6,
+    z = 326.18,
+    heading = 236.09,
     model = 'player_one',
     imageUrl = 'https://picsum.photos/512?a=2',
-    label = 'Route68Bunker'
-}
-
-loc 'random-3' {
-    x = 481.0465,
-    y = 2995.135,
-    z = 43.96672,
-    heading = 0,
-    model = 'player_one',
-    imageUrl = 'https://picsum.photos/512?a=3',
-    label = 'OilfieldsBunker'
-}
-
-loc 'random-4' {
-    x = 848.6175,
-    y = 2996.567,
-    z = 45.81612,
-    heading = 0,
-    model = 'player_two',
-    imageUrl = 'https://picsum.photos/512?a=4',
-    label = 'DesertBunker'
-}
-
-loc 'random-5' {
-    x = 2126.785,
-    y = 3335.04,
-    z = 48.21422,
-    heading = 0,
-    model = 'player_zero',
-    imageUrl = 'https://picsum.photos/512?a=5',
-    label = 'SmokeTreeBunker'
+    label = 'Pillbox Hill'
 }
